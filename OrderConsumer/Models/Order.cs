f488f1e6ba7a33c2059c17387fdbbdc99f959efb
gosha <goshaberishvili@yandex.ru>
2026-02-11 18:00:03 +0300

    создал consumer, добавил модель заказа

---
+++ OrderConsumer/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderConsumer.Models;
+
+public sealed class Order
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = null!;
+    public decimal Amount { get; set; }
+    public string IdempKey { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+}
