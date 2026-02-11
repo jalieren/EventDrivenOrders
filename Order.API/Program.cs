6baab23a579e3ab751b21377f174909eaa15cc84
gosha <goshaberishvili@yandex.ru>
2026-02-11 03:37:40 +0300

    добавил логи и валидацию

--- Order.API/Program.cs
+++ Order.API/Program.cs
@@ -17,9 +17,25 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/health", () => Results.Ok("OK"));
 
-app.MapPost("/orders", (CreateOrderRequest request) =>
+app.MapPost("/orders", (CreateOrderRequest request, ILogger<Program> logger) =>
     {
+        // валидация
+        if (string.IsNullOrWhiteSpace(request.CustomerEmail) || !request.CustomerEmail.Contains("@"))
+            return Results.BadRequest(new { Error = "Неверный email" });
+
+        if (request.Amount <= 0) 
+            return Results.BadRequest(new { Error = "Сумма должна быть больше нуля" });
+        
+        // логируем получение
+        logger.LogInformation("Получен новый заказ от {Email} на сумму {Amount}", 
+            request.CustomerEmail, request.Amount);
+        
+        // генерация id
         var orderId = Guid.NewGuid();
+        
+        // логируем генерацию id
+        logger.LogInformation("Сгенерирован ID заказа {orderID} от {Email}", 
+            orderId, request.CustomerEmail);
 
         return Results.Ok(new { OrderId = orderId });
     }
